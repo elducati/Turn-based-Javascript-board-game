474b3991e70fa972898acfa1dd59e47244a69b55
elducati <geomondi09@gmail.com>
2020-08-23 19:09:11 +0300

    added functionality for mouseenter

--- js/board.game.js
+++ js/board.game.js
@@ -1,9 +1,10 @@
 class BoardGame {
     constructor(selector){
-        this.ROWS = 10;
+        this.ROWS = 6;
         this.COLS = 10;
         this.selector = selector;
         this.createGrid();
+        this.setupEventListeners();
     }
     createGrid(){
         const $board = $(this.selector);
@@ -13,11 +14,31 @@ class BoardGame {
             .addClass('row');
             for(let col = 0; col < this.COLS; col++){
                 const $col = $('<div>')
-                .addClass('col empty');
+                .addClass('col empty')
+                .attr('data-col', col)
+                .attr('data-row', row);
                 $row.append($col);
             }
             $board.append($row);
         }
         console.log($board.html());
+    }
+    setupEventListeners(){
+        const $board = $(this.selector);
+        function findLastEmptyCell(col){
+            const cells = $(`.col[data-col='${col}']`);
+            for (let i = cells.length - 1; i >= 0; i--){
+                const $cell = $(cells[i]);
+                if ($cell.hasClass('empty')){
+                    return $cell;
+                }
+            }
+            return null;
+        }
+        $board.on('mouseenter', '.col.empty', function(){
+            const col = $(this).data('col');
+            const $lastEmptyCell = findLastEmptyCell(col);
+            $lastEmptyCell.addClass('autobot');
+        })
     }
 }
