e7ef3e45a831837ca01fc49c21f0ad78d03e2659
elducati <geomondi09@gmail.com>
2020-09-06 13:04:34 +0300

    added classes to the game

--- js/board.game.js
+++ js/board.game.js
@@ -1,8 +1,8 @@
 class BoardGame {
     constructor(selector) {
-        this.ROWS = 6;
-        this.COLS = 6;
-        this.player = 'cybertron'
+        this.ROWS = 8;
+        this.COLS = 11;
+        this.player = player;
         this.selector = selector;
         this.createGrid();
         this.setupEventListeners();
@@ -18,43 +18,12 @@ class BoardGame {
                     .addClass('col empty')
                     .attr('data-col', col)
                     .attr('data-row', row);
+                    
                 $row.append($col);
             }
             $board.append($row);
         }
         console.log($board.html());
     }
-    setupEventListeners() {
-        const $board = $(this.selector);
-        const that = this;
-        function findLastEmptyCell(col) {
-            const cells = $(`.col[data-col='${col}']`);
-
-            for (let i = cells.length - 1; i >= 0; i--) {
-                const $cell = $(cells[i]);
-                if ($cell.hasClass('empty')) {
-                    return $cell;
-                }
-            }
-            return null;
-        }
-        $board.on('mouseenter', '.col.empty', function () {
-            const col = $(this).data('col');
-            const $lastEmptyCell = findLastEmptyCell(col);
-            $lastEmptyCell.addClass(`${that.player}`);
-            
-            
-        })
-        $board.on('mouseleave', '.col', function(){
-            $('.col').removeClass(`${that.player}`);
-        })
-        $board.on('click', '.col.empty', function(){
-            const col = $(this).data('col');
-            const $lastEmptyCell = findLastEmptyCell(col);
-            $lastEmptyCell.removeClass(`empty ${that.player}`);
-            $lastEmptyCell.addClass(that.player);
-            that.player = (that.player === 'cybertron') ? 'autobot' : 'cybertron';
-            
-        })
-    }
+    
 }
---
+++ js/weapon.js
@@ -0,0 +1,7 @@
+class Weapon {
+    constructor(){
+        this.type = type;
+        this.value = value;
+        this. itemDataAttribute = itemDataAttribute;
+    }
+}
